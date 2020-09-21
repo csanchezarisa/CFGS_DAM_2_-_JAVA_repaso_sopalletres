53a4929e3014aa15f57165d14f4367fc2758985c
csanchezarisa <cristobal.sanchez@mataro.epiaedu.cat>
2020-09-21 22:48:39 +0200

    Opciones de menú 'b' y 'c' implantadas. Ahora se pueden hacer busquedas de palabras con extension de 1 y 2 letras

--- src/main.java
+++ src/main.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class main {
 
-    public final int MIDATAULER = 10;
+    public final int MIDATAULER = 12;
     public char[][] tauler = new char[MIDATAULER][MIDATAULER];
     public Scanner teclat = new Scanner(System.in);
 
     /* Llanca el programa principal */
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
         main launch = new main();
         launch.launcher();
     }
 
     /* .: 1 :. Metode principal */
-    public void launcher() {
+    public void launcher() throws InterruptedException {
         omplenarTauler();
 
         char opcioUsuari = ' ';
         do {
 
+            netejarPantalla();
             imprimirTauler();
             menuPrincipal();
             System.out.print("Escull una opció: ");
@@ -32,9 +33,11 @@ public class main {
                     break;
 
                 case 'b':
+                    buscarParaula(false);
                     break;
 
                 case 'c':
+                    buscarParaula(true);
                     break;
 
                 case 'd':
@@ -63,8 +66,8 @@ public class main {
 
     /* S'encarrega d'omplenar amb lletres random el tauler */
     private void omplenarTauler() {
-        for (int index = 0; index < MIDATAULER; index++) {
-            for (int subIndex = 0; subIndex < MIDATAULER; subIndex++) {
+        for (int index = 1; index < MIDATAULER - 1; index++) {
+            for (int subIndex = 1; subIndex < MIDATAULER - 1; subIndex++) {
                 tauler[index][subIndex] = (char) ((Math.random() * 26) + 97);
             }
         }
@@ -72,16 +75,89 @@ public class main {
 
     /* Mostra el tauler per pantalla */
     private void imprimirTauler() {
-        for (int index = 0; index < MIDATAULER; index++) {
-            for (int subIndex = 0; subIndex < MIDATAULER; subIndex++)
+        for (int index = 1; index < MIDATAULER - 1; index++) {
+            for (int subIndex = 1; subIndex < MIDATAULER - 1; subIndex++)
                 System.out.print(tauler[index][subIndex] + " ");
             System.out.println();
         }
+    }
+
+    /* .: 2 :. Buscar paraula en el tauler */
+    /* Aquest metode ha de rebre un boolean, aquest decideix si, quan troba la paraula, deixa de buscar-ne mes repeticions o hi continua */
+    private void buscarParaula(boolean continuat) throws InterruptedException {
+
+        netejarPantalla();
+        imprimirTauler();
+        System.out.print("Paraula a cercar: ");
+        String paraula = teclat.next();
+        if (paraula.length() >= 1) {
+            cercaParaula(continuat, paraula);
+        }
+        else {
+            System.out.println("Introdueix una paraula valida...");
+            stop();
+        }
+
+    }
+
+    /* Busca en tot el tauler si hi figura la primera lletra de la paraula a cercar */
+    private void cercaParaula(boolean continuat, String paraulaPerCercar) {
+        boolean existeix = false;
+        for (int fila = 1; fila < MIDATAULER - 1; fila++) {
+            for (int columna = 1; columna < MIDATAULER - 1; columna++) {
+                if (tauler[fila][columna] == paraulaPerCercar.charAt(0)) {
+                    existeix = false;
+
+                    if (paraulaPerCercar.length() == 1) {
+                        existeix = true;
+                    }
+                    else {
+                        existeix = revisarVoltant(fila, columna, paraulaPerCercar);
+                    }
+
+                    if (existeix) {
+                        tauler[fila][columna] = Character.toUpperCase(tauler[fila][columna]);
+
+                        if (!continuat) {
+                            fila = MIDATAULER;
+                            columna = MIDATAULER;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean revisarVoltant(int fila, int columna, String paraulaPerCercar) {
+        boolean existeix = false;
+
+        for (int filaPerRevisar = fila - 1; filaPerRevisar <= fila + 1; filaPerRevisar++) {
+            for (int columnaPerRevisar = columna - 1; columnaPerRevisar <= columna + 1; columnaPerRevisar++) {
+                if (tauler[filaPerRevisar][columnaPerRevisar] == paraulaPerCercar.charAt(1) && !(filaPerRevisar == fila && columnaPerRevisar == columna)) {
+                    if (paraulaPerCercar.length() == 2) {
+                        existeix = true;
+                        tauler[filaPerRevisar][columnaPerRevisar] = Character.toUpperCase(tauler[filaPerRevisar][columnaPerRevisar]);
+                        columnaPerRevisar = columna + 1;
+                        filaPerRevisar = fila + 1;
+                    }
+                    else {
+
+                    }
+                }
+            }
+        }
+
+        return existeix;
     }
 
     /* Neteja la pantalla */
     public void netejarPantalla() {
         for (int index = 0; index < 50; index++)
             System.out.println();
+    }
+
+    /* Mètode que para el programa i el deixa atorat */
+    public void stop() throws InterruptedException {
+        Thread.sleep(10000);
     }
 }
