fa6d0b7ccbd89c89f623e839c99fe1561e53703f
ulkiobal <ulkiobal@users.noreply.github.com>
2020-09-21 19:03:05 +0200

    Mostrar menú principal y opción 'a' para rehacer el tablero

--- src/main.java
+++ src/main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class main {
 
     public final int MIDATAULER = 10;
     public char[][] tauler = new char[MIDATAULER][MIDATAULER];
+    public Scanner teclat = new Scanner(System.in);
 
     /* Llanca el programa principal */
     public static void main(String args[]) {
@@ -12,23 +15,73 @@ public class main {
     /* .: 1 :. Metode principal */
     public void launcher() {
         omplenarTauler();
-        imprimirTauler();
+
+        char opcioUsuari = ' ';
+        do {
+
+            imprimirTauler();
+            menuPrincipal();
+            System.out.print("Escull una opció: ");
+            opcioUsuari = Character.toLowerCase(teclat.next().charAt(0));
+
+            switch (opcioUsuari) {
+
+                case 'a':
+                    omplenarTauler();
+                    netejarPantalla();
+                    break;
+
+                case 'b':
+                    break;
+
+                case 'c':
+                    break;
+
+                case 'd':
+                    System.out.println("Sortint...");
+                    break;
+
+                default:
+                    System.out.println("Escull una opció vàlida!");
+                    break;
+
+            }
+
+        }
+        while (opcioUsuari != 'd');
+
+    }
+
+    /* Mostra el menú principal */
+    private void menuPrincipal() {
+        System.out.println();
+        System.out.println("a - Refer sopa de lletres");
+        System.out.println("b - Buscar paraula 1 vegada");
+        System.out.println("c - Buscar paraula totes les possibles");
+        System.out.println("d - Sortir");
     }
 
     /* S'encarrega d'omplenar amb lletres random el tauler */
     private void omplenarTauler() {
         for (int index = 0; index < MIDATAULER; index++) {
             for (int subIndex = 0; subIndex < MIDATAULER; subIndex++) {
-                tauler[index][subIndex] = (char) ((Math.random() * 25) + 97);
+                tauler[index][subIndex] = (char) ((Math.random() * 26) + 97);
             }
         }
     }
 
+    /* Mostra el tauler per pantalla */
     private void imprimirTauler() {
         for (int index = 0; index < MIDATAULER; index++) {
             for (int subIndex = 0; subIndex < MIDATAULER; subIndex++)
                 System.out.print(tauler[index][subIndex] + " ");
             System.out.println();
         }
+    }
+
+    /* Neteja la pantalla */
+    public void netejarPantalla() {
+        for (int index = 0; index < 50; index++)
+            System.out.println();
     }
 }
